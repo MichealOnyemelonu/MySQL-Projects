51d499f28c01fcd046c58449023cd4fb0383e66a
Micheal Onyemelonu <monyemelonu@gmail.com>
2024-05-11 09:16:05 +0200

    Add files via upload

---
+++ SQL Personal Projects.sql
@@ -0,0 +1,9 @@
+select employee_id, 
+last_name, 
+birth_date,
+age,
+age + 10
+from parks_and_recreation.employee_demographics;
+
+
+Select distinct first_name, gender from parks_and_recreation.employee_demographics;
